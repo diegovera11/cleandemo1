d156a1a8e9bf6fe69b01ca0cd29e0acc0a5e45ae
diegovera11 <diegovera11@gmail.com>
2023-01-09 10:30:40 -0500

    Initial version Spring Initializr

---
+++ src/main/java/com/diegovera/cleandemo1/Cleandemo1Application.java
@@ -0,0 +1,13 @@
+package com.diegovera.cleandemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cleandemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cleandemo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/diegovera/cleandemo1/Cleandemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegovera.cleandemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cleandemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
